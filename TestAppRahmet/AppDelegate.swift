72a53ad0eef8c413eace7d091c50bceb137a752d
Ulan Nurmamatov <ulannurmamatov@gmail.com>
2019-02-02 22:42:01 +0600

    initial commit

---
+++ TestAppRahmet/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TestAppRahmet
+//
+//  Created by Ulan Nurmamatov on 2/2/19.
+//  Copyright © 2019 Ulan Nurmamatov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+}
+
